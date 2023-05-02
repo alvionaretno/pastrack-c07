4ac0ca422d582338d12185235a4192b2cf7f4e7c
Dyta Dewipuspita <dyta.dewipuspita01@ui.ac.id>
2023-05-02 14:08:36 +0700

    Feat/dasboard guru/dyta dewipuspita

--- src/main/java/com/PASTRACK/PASTRACK/KelasRequest/addKelasResponse.java
+++ src/main/java/com/PASTRACK/PASTRACK/KelasRequest/addKelasResponse.java
@@ -2,6 +2,7 @@ package com.PASTRACK.PASTRACK.KelasRequest;
 
 import com.PASTRACK.PASTRACK.Model.MataPelajaranModel;
 import com.PASTRACK.PASTRACK.Model.StudentModel;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 
 import java.util.List;
@@ -14,8 +15,11 @@ public class addKelasResponse {
     Long semesterId;
     String usernameGuru;
 
+
+    @JsonIgnore
     List<StudentModel> listMurid;
 
+    @JsonIgnore
     List<MataPelajaranModel> listMataPelajaran;
 
     public addKelasResponse (Long idKelas, String namaKelas, Long semesterId, String usernameGuru, List<StudentModel> listMurid, List<MataPelajaranModel> listMataPelajaran) {
